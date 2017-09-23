c07f9fa6a3578158b58150a709e5e96adab7d119
Maho Morichika <nat@magicnat.com>
2017-09-23 11:23:49 -0400

    still try gyroscope if it exist.

--- assets/js/background.js
+++ assets/js/background.js
@@ -6,6 +6,27 @@ var anim_layers = function (layers) {
 
   var layers = layers.map(layer => document.getElementById(layer));
 
+  var rMap = {
+    0: [[1, 0, 0], [0, -1, 0]],
+    90: [[0, -1, 0], [-1, 0, 0]],
+    180: [[-1, 0, 0], [0, 1, 0]],
+    270: [[0, 1, 0], [1, 0, 0]]
+  };
+
+  var getZ = (x, y, z, beta, gamma) => (z*Math.cos(beta*Math.PI/180)+y*Math.sin(beta*Math.PI/180))*Math.cos(gamma*Math.PI/180)-x*Math.sin(gamma*Math.PI/180);
+
+  if (window.DeviceMotionEvent) {
+    window.addEventListener("deviceorientation", e => {
+      var rot = window.orientation;
+      rot = rot == -90 ? 270 : rot;
+
+      if (e.beta || e.gamma) {
+        fX = () => getZ(rMap[rot][0][0], rMap[rot][0][1], rMap[rot][0][2], e.beta, e.gamma);
+        fY = () => getZ(rMap[rot][1][0], rMap[rot][1][1], rMap[rot][1][2], e.beta, e.gamma);
+      }
+    });
+  }
+
   var rndr = function () {
     var tX = .05 * window.innerWidth * fX(2, deg),
         tY = .05 * window.innerHeight * fY(2, deg);
